87f37bd45586f2042c9466c0ce56a00cc5d3ab4d
엄청식 <viveloper@eomcheongsigs-MacBook-Pro.local>
2020-01-11 15:04:26 +0900

    updated dbschema

--- dbschema.js
+++ dbschema.js
@@ -1,8 +1,20 @@
 const db = {
+  users: [
+    {
+      userId: 'gafghffsvxcga',
+      email: 'user@email.com',
+      handle: 'user',
+      createdAt: '2020-01-10T03:35:54.961Z',
+      imageUrl: 'https://skakgkskdslf.com',
+      bio: 'Hello, my name is user, nice to meet you',
+      website: 'http://user.com',
+      location: 'London, UK'
+    }
+  ],
   screams: [
     {
       userHandle: 'user',
-      body: 'this is the scream body',
+      body: 'this is a sample scream',
       createdAt: '2020-01-10T03:35:54.961Z',
       likeCount: 5,
       commentCount: 2
